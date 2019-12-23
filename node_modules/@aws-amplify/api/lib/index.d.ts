bdb76465d4a3c67fb96a90fe757c9cb5ce0c0e39
Mitesh Dhagai <mitayshd@amazon.com>
2019-12-23 10:40:55 +0530

    conflict

---
+++ node_modules/@aws-amplify/api/lib/index.d.ts
@@ -0,0 +1,5 @@
+import APIClass, { graphqlOperation } from './API';
+import { GraphQLResult } from './types';
+declare const API: APIClass;
+export default API;
+export { APIClass, graphqlOperation, GraphQLResult };
